a99c389b064387893553ce373710160bcc911325
PRASHANT TIWARI <111697531+ursprash@users.noreply.github.com>
2023-10-06 20:30:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+import {
+    clusterApiUrl,
+    Connection,
+    PublicKey,
+    Keypair,
+    LAMPORTS_PER_SOL,
+  } from "@solana/web3.js";
+  import {
+    createMint,
+    getOrCreateAssociatedTokenAccount,
+    mintTo,
+    transfer,
+  } from "@solana/spl-token";
+  
+  (async () => {
+    // Step 1: Connect to cluster and generate a new Keypair
+    const newPair = new Keypair();
+    const publicKey = new PublicKey(newPair.publicKey).toString();
+    const privateKey = newPair.secretKey.toString(); // Get the private key as a string
+    console.log('public key:', publicKey);
+    console.log('private key:', privateKey); // Print the private key
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const fromWallet = Keypair.generate();
+    const toWallet = Keypair.generate();
 
+    const fromAirdropSignature = await connection.requestAirdrop(
+    fromWallet.publicKey,
+    LAMPORTS_PER_SOL
+  );
+  // Wait for airdrop confirmation
+  await connection.confirmTransaction(fromAirdropSignature, {
+    commitment: "confirmed",
+  });
+  })();
+  
